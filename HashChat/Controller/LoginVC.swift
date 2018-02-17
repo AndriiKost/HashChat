c8c742e9a124164e62197aae826743a992e7dcca
Andrii Kost <andriikost@yahoo.com>
2018-02-17 10:43:28 -0600

    set up UI for LoginVC with constraints

---
+++ HashChat/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  HashChat
+//
+//  Created by Andrii Kost on 2/17/18.
+//  Copyright © 2018 Andrii Kost. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closeButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
---
+++ HashChat/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  HashChat
+//
+//  Created by Andrii Kost on 2/17/18.
+//  Copyright © 2018 Andrii Kost. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "goToLoginVC"
